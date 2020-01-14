31b4e4487717a55db7a35169775ce1dd1c15b37e
archoad <myshell.dubois@gmail.com>
2020-01-14 11:04:15 +0100

    Securing folder access

---
+++ pict/index.php
@@ -0,0 +1,8 @@
+<?php
+	$temp = explode(DIRECTORY_SEPARATOR, $_SERVER['SCRIPT_NAME']);
+	$url = '';
+	for ($i=0; $i<=array_search('evalsmsi', $temp); $i++) {
+		$url .= $temp[$i].DIRECTORY_SEPARATOR;
+	}
+	header('Location: '.$url);
+?>
