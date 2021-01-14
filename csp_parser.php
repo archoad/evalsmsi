da3bbe066a1dd2ffc27afcfa603d5cf173534df3
archoad <myshell.dubois@gmail.com>
2021-01-14 15:08:14 +0100

    Bugs correction

--- csp_parser.php
+++ csp_parser.php
@@ -4,11 +4,12 @@ include("functions.php");
 header('X-Content-Type-Options: "nosniff"');
 global $progVersion;
 $log = array();
-$log[] = array('program' => 'evalsmsi', 'version' => $progVersion);
-$log[] = array('function' => 'csp_report');
+$log['program'] = 'evalsmsi';
+$log['version'] = $progVersion;
+$log['function'] = 'csp_report';
 if ($data = file_get_contents('php://input')) {
 	if ($data = json_decode($data, true)) {
-		$log[] = json_encode($data, JSON_UNESCAPED_SLASHES);
+		$log['data'] = json_encode($data, JSON_UNESCAPED_SLASHES);
 		openlog("evalsmsi", LOG_PID, LOG_SYSLOG);
 		syslog(LOG_INFO, json_encode($log));
 		closelog();
